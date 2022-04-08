b7a84771b7061c3f66dda44fac29dd558e865245
Johan Edman <22528843+EdmanJohan@users.noreply.github.com>
2022-04-08 17:37:15 +0200

    Update produce.c

--- tutorial_5_hooks/container/produce/produce.c
+++ tutorial_5_hooks/container/produce/produce.c
@@ -1,8 +1,19 @@
 #include "bpf/bpfapi/helpers.h"
 
-static const char print_str[] = "Produce\n";
+#define SHARED_KEY 0x50
+
+static const char print_str[] = "Produce: %d\n";
 
 void produce(void)
 {
-    bpf_printf(print_str);
+    uint32_t value;
+    bpf_fetch_global(SHARED_KEY, &value);
+
+    if (value < UINT16_MAX) {
+        value++;
+    }
+
+    bpf_store_global(SHARED_KEY, value);
+
+    bpf_printf(print_str, value);
 }
